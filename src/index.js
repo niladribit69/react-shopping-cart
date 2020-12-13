39113ac45d6ac8a6c44871636a3b7e3a6021966e
Your Name <niladri.bit@rediffmail.com>
2020-12-13 09:25:35 +0530

    feature 1

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-//hello
-
+// helloworld
 ReactDOM.render(
   <React.StrictMode>
     <App />
